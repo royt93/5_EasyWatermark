cbd0463961d701627d48dbaefd9290d8625c3c8c
roy <loitp@galaxyjoy.vn>
2025-11-06 22:29:48 +0700

    feat: Update application version
    - In `app/build.gradle.kts`, update `versionCode` to 20251106 and `versionName` to "2025.11.06".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.mckimquyen.watermark"
         minSdk = 23
         targetSdk = 36
-        versionCode = 20251105
-        versionName = "2025.11.05"
+        versionCode = 20251106
+        versionName = "2025.11.06"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
     }
