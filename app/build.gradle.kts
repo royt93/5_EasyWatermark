208a14da5cb7bfdb57e3f858f1272b87a7a78422
roy <loitp@galaxyjoy.vn>
2026-02-16 15:57:19 +0700

    feat: Update application version and Gradle properties
    - In `app/build.gradle.kts`, update `versionCode` to `20260216` and `versionName` to `"2026.02.16"`.
    - In `gradle.properties`, add `kotlin.daemon.jvm.options` and set `kapt.use.worker.api` to `false`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.mckimquyen.watermark"
         minSdk = 23
         targetSdk = 36
-        versionCode = 20251106
-        versionName = "2025.11.06"
+        versionCode = 20260216
+        versionName = "2026.02.16"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
     }
